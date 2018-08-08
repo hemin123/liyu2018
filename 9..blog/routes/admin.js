d020f72fd3c60b531037e6c3d42a33f09b3ec168
hemin123 <1565417293@qq.com>
2018-08-08 16:50:14 +0800

    8.8

--- 9..blog/routes/admin.js
+++ 9..blog/routes/admin.js
@@ -1,6 +1,14 @@
 const Router = require('express').Router;
-
+const UserModel = require('../models/user.js');
 const router = Router();
+
+router.use((req,res,next)=>{
+	if (req.userInfo.isAdmin){
+		next();
+	} else{
+		res.send('<h1>使用管理员账号登陆</h1>')
+	}
+})
 
 //显示首页
 router.get("/",(req,res)=>{
@@ -10,5 +18,18 @@ router.get("/",(req,res)=>{
 	});
 	// res.end("hhhh");
 })
+
+router.get('/users',(req,res)=>{
+	UserModel.find({},'_id username isAdmin')
+	.then((user)=>{
+		res.rnder('admin/user_list',
+			{userInfo:req.userInfo,
+				users:users});
+
+
+	})
+	
+})
+
 
 module.exports = router;
